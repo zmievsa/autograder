af6f82534c1806b4de9802a9e013ececdddfb733
Ovsyanka83 <szmiev2000@gmail.com>
2020-03-01 21:56:45 -0500

    Added custom input

--- C/grader/grader.py
+++ C/grader/grader.py
@@ -2,16 +2,20 @@ import sh
 from pathlib import Path
 import shutil
 from timer import Timer
-import filecmp
 from typing import List
 from multiprocessing import Pool
 from collections import namedtuple
+from io import StringIO
 
 # CONFIG
 TIMEOUT = 1
-SOURCE_FILE_NAME = "homework1.c"  # A string that has to be in each source file name
-ASSIGNMENT_NAME = "Homework 1"  # For display in the output
-total_possible_points = 60
+ERASE_PRINTF = False
+ERASE_SCANF  = False  # can't do this yet
+RENAME_STUDENT_MAIN = True
+COMPILATION_ARGUMENTS: List[str] = []
+SOURCE_FILE_NAME = "homework1.c"    # A string that has to be in each source file name
+ASSIGNMENT_NAME = "Homework 1"      # For display in the output
+TOTAL_POINTS_POSSIBLE = 100
 
 # Constants
 KEY = """
@@ -23,26 +27,52 @@ KEY = """
 \tExceeded Time Limit: Your submission took longer than 60 seconds to run (probably an infinite loop)
 \tWrong Answer!: Your submission produced the wrong result or generated too much output
 """
+TEMP_FILE_SUFFIXES = (".c", ".o", ".exe", ".txt")
 
 
 CURRENT_DIR = Path(__file__).parent.resolve()
-REMOVE_MAIN = True
-REMOVE_PRINTF = True
+submissions_dir: Path = CURRENT_DIR / "submissions"
+results_dir: Path = CURRENT_DIR / "results"
+tests_dir: Path = CURRENT_DIR / "tests"
 
 
 class Test:
     def __init__(self, path: Path):
         self.path = path
+        with open(tests_dir / f"output/{path.stem}.txt") as f:
+            self.expected_output = format_output(f.read())
+        with open(tests_dir / f"input/{path.stem}.txt") as f:
+            self.input = StringIO(f.read().strip())
+        
+        # Only really works if test is in snake_case
+        self.name = path.stem.replace("_", " ").capitalize()
     
-    def expected_output_path(self):
-        return 
+    def run(self, submission):
+        executable_path = make_executable_path(self, submission)
+        try:
+            sh.gcc("-o", executable_path, self, submission.stem + ".o")  # In case we want to do non-background compilation
+        except sh.ErrorReturnCode as e:
+            return "Failed to Compile!"
+        runtime_output = StringIO()
+        test_executable = sh.Command(executable_path)
+        try:
+            test_executable(_in=self.input, _out=runtime_output, _timeout=TIMEOUT)
+        except sh.TimeoutException:
+            return "Exceeded Time Limit"
+        except sh.ErrorReturnCode:
+            return "Crashed"
+        if format_output(runtime_output.read()) == self.expected_output:
+            return "PASS"
+        else:
+            return "Wrong Answer!"
+    
+    def make_executable_path(self, submission: Path):
+        return self.path.with_name(self.path.stem + submission.stem + ".exe")
 
 
 def main():
     with Timer(lambda t: print(f"It took us {t} seconds")):
         sh.cd(CURRENT_DIR)
-        submissions_dir: Path = CURRENT_DIR / "submissions"
-        results_dir: Path = CURRENT_DIR / "results"
         results_dir.mkdir(exist_ok=True)
         tests_dir = CURRENT_DIR / "tests/testcases"
         clean_directory(CURRENT_DIR)
@@ -51,24 +81,26 @@ def main():
         with Pool() as p:
             total_class_points = sum(p.map(run_tests_on_submission, submissions))
         class_average = total_class_points / (len(submissions) or 1)
-        print(f"\nAverage score: {round(class_average)}/{total_possible_points}")
+        print(f"\nAverage score: {round(class_average)}/{TOTAL_POINTS_POSSIBLE}")
         clean_directory(CURRENT_DIR)
+        for test in tests:
+            test.input.close()
 
 
 def clean_directory(directory: Path):
     for path in directory.iterdir():
         ext = path.suffix
-        if ext == ".c" or ext == ".o" or ext == ".exe" or ext == ".txt":
+        if any(ext == s for s in TEMP_FILE_SUFFIXES):
             path.unlink()
 
 
-def gather_tests(tests_dir: Path) -> List[Path]:
+def gather_tests(tests_dir: Path) -> List[Test]:
     tests = []
     for test in tests_dir.iterdir():
         if not test.is_file():
             continue
         shutil.copy(test, CURRENT_DIR)
-        tests.append(CURRENT_DIR / test.name)
+        tests.append(Test(CURRENT_DIR / test.name))
     return tests
 
 
@@ -83,19 +115,18 @@ def run_tests_on_submission(args):
         f.write("%-40s%s" % ("TestCase", "Result"))
         f.write("\n================================================================")
         try:
-            sh.gcc("-c", f"{submission.name}", "-Dmain=__student_main__", "-Dprintf(...);=")
+            sh.gcc("-c", f"{submission.name}", *COMPILATION_ARGUMENTS)
         except sh.ErrorReturnCode_1 as e:
             f.write("\nYour file failed to compile!")
             return 0
         test: Path
         pass_count = 0
         for test in tests:
-            test_result = run_test(test, submission)
-            test_name = test.stem.replace("_", " ").capitalize()
-            f.write("\n%-40s%s" % (test_name, test_result))
+            test_result = test.run(submission)
+            f.write("\n%-40s%s" % (test.name, test_result))
             pass_count += int(test_result == "PASS")
-        student_score = pass_count * total_possible_points / testcase_count
-        student_final_result = f"{round(student_score)}/{total_possible_points}"
+        student_score = pass_count * TOTAL_POINTS_POSSIBLE / testcase_count
+        student_final_result = f"{round(student_score)}/{TOTAL_POINTS_POSSIBLE}"
         print(f"{student_name}.", student_final_result)
         f.write("\n================================================================\n")
         f.write("Result: " + student_final_result)
@@ -103,38 +134,10 @@ def run_tests_on_submission(args):
         return student_score
 
 
-def make_test_executable_path(test: Path, student_submission: Path):
-    return test.with_name(test.stem + student_submission.stem + ".exe")
+def format_output(output: str):
+    """ Removes whitespace and lowers """
+    return "".join(output.lower().split())
 
 
-def run_test(test: Path,  submission: Path):
-    try:
-        sh.gcc("-o", make_test_executable_path(test, submission), test, submission.stem + ".o")  # In case we want to do non-background compilation
-    except sh.ErrorReturnCode as e:
-        return "Failed to Compile!"
-    with open(submission.stem + "_output.txt", "w") as runtime_output:
-        test_executable = sh.Command(make_test_executable_path(test, submission))
-        try:
-            test_executable(_in=test.input,_out=runtime_output, _timeout=TIMEOUT)
-        except sh.TimeoutException:
-            return "Exceeded Time Limit"
-        except sh.ErrorReturnCode:
-            return "Crashed"
-    if filecmp.cmp(submission.stem + "_output.txt", (CURRENT_DIR / f"tests/output/{test.stem}.txt")):
-        return "PASS"
-    else:
-        return "Wrong Answer!"
-
-
-def compile_tests(tests: List[Path], submission: Path):
-    """ An optimization to compiling that's supposed to make it faster but doesn't benefit us
-        if we're already using multiprocessing. Don't forget to call .wait() on compiled tests
-    """
-    compiled_tests = []
-    for test in tests:
-        test_executable_path = make_test_executable_path(test, submission)
-        compiled_tests.append(sh.gcc("-o", test_executable_path, test, submission.stem + ".o", _bg=True, _bg_exc=False))
-    return compiled_tests
-
-
-main()
+if __name__ == "__main__":
+    main()
