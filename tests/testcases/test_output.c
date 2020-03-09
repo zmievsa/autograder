c877e97a71243d210afc52367512345562f138e1
Ovsyanka83 <szmiev2000@gmail.com>
2020-03-09 11:44:38 -0400

    Add prototyping student function in testcases

--- tests/testcases/test_output.c
+++ tests/testcases/test_output.c
@@ -1,3 +1,5 @@
+int numberSaver();
+
 int main() {
     numberSaver();
     NO_RESULT();
--- tests/testcases/test_result.c
+++ tests/testcases/test_result.c
@@ -1,5 +1,7 @@
 // This is a made up example of a test case that demonstrates the functionality of the grader
 
+int numberSaver();
+
 int main() {
     int res = numberSaver();
     if (res == 54)
