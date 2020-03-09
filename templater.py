94c8963032acab7e4dcd4098a51c12fc37a48a67
Stanislav Zmiev <zertarx@gmail.com>
2020-03-08 21:01:10 -0400

    Add files via upload

---
+++ templater.py
@@ -0,0 +1,16 @@
+import re
+
+matcher = re.compile("{% *([A-Za-z0-9_]+) *%}")
+
+def format_template(template, **kwargs):
+    for match in matcher.finditer(template):
+        attr = match.group(1)
+        value = kwargs.pop(attr, None)
+        if value is None:
+            raise ValueError(f"Attribute {attr} not supplied")
+        template = template.replace(match.group(0), str(value))
+    if len(kwargs):
+        raise ValueError("Too many arguments supplied: " + ", ".join(kwargs.keys()))
+    return template
+
+print(format_template("suka bluad pizda {% num %}, {% num2   %},", num=78, num2=-9))
