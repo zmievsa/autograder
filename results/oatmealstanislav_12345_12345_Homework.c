2064ff65b45ed71a9deb44d8c430f7592f3ebf3e
Ovsyanka83 <szmiev2000@gmail.com>
2020-03-09 09:31:19 -0400

    Results

---
+++ results/oatmealstanislav_12345_12345_Homework.c
@@ -0,0 +1,14 @@
+Homework Test Results
+
+TestCase                                Result
+================================================================
+Test output                             100/100
+Test result                             100/100
+================================================================
+Result: 100/100
+
+Key:
+	Failed to Compile: Your submission did not compile due to a syntax or naming error
+	Compiled with warnings: Your submission uses unchecked or unsafe operations
+	Crashed: Your submission threw an uncaught exception
+	Exceeded Time Limit: Your submission took longer than 60 seconds to run (probably an infinite loop)
