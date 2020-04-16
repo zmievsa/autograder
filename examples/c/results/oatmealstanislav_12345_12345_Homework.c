2e403c2461deaf47597617ccb5f216be0497fa4e
Ovsyanka83 <szmiev2000@gmail.com>
2020-04-15 23:22:55 -0400

    Change KEY a little bit

--- examples/c/results/oatmealstanislav_12345_12345_Homework.c
+++ examples/c/results/oatmealstanislav_12345_12345_Homework.c
@@ -12,4 +12,4 @@ Key:
 	Compiled with warnings: Your submission uses unchecked or unsafe operations
 	Crashed due to signal SIGNAL_CODE: Your submission threw an uncaught exception.
 	All signal error codes are described here: http://man7.org/linux/man-pages/man7/signal.7.html
-	Exceeded Time Limit: Your submission took longer than 60 seconds to run (probably an infinite loop)
+	Exceeded Time Limit: Your submission took too much time to run (probably an infinite loop)
