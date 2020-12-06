0ebb3b77bf7f08d1da65d33cc77d195e1c2eab7d
Stanislav Zmiev <szmiev2000@gmail.com>
2020-12-06 13:51:49 -0500

    Update test_helper.c

--- autograder/test_helpers/test_helper.c
+++ autograder/test_helpers/test_helper.c
@@ -5,7 +5,7 @@
 
 
 static int RESULT_EXIT_CODES[] = {{% RESULT_EXIT_CODES %}};
-
+// TODO: Change these into static functions for security (done in dev branch)
 #define CHECK_OUTPUT() exit({% CHECK_OUTPUT_EXIT_CODE %})
 #define RESULT(r) exit(RESULT_EXIT_CODES[r])
 #define PASS() RESULT(100)
