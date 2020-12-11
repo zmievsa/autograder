4d10de89134e1b073e8f5543b5c306fdfbdd3a69
Stanislav Zmiev <szmiev2000@gmail.com>
2020-12-11 12:41:31 -0500

    Fix --guide option not working

--- autograder/__main__.py
+++ autograder/__main__.py
@@ -4,6 +4,7 @@ from pathlib import Path
 
 from autograder.grader import Grader  # That's some awful naming
 from autograder.util import print_results, AutograderError
+from autograder import guide
 from autograder.__version__ import __version__
 
 
@@ -12,34 +13,38 @@ def main(argv=None):
     if argv is None:
         argv = sys.argv[1:]
     parser = argparse.ArgumentParser()
-    parser.add_argument('-v', '--version',
-        action='store_true',
-        help='show the version number'
+    parser.add_argument("-v", "--version", action="store_true", help="show the version number")
+    parser.add_argument(
+        "submission_path",
+        type=Path,
+        nargs="?",
+        default=Path.cwd(),
+        help="Path to directory that contains student submissions",
     )
-    parser.add_argument('submission_path',
-        type=Path, nargs="?", default=Path.cwd(),
-        help='Path to directory that contains student submissions'
+    parser.add_argument(
+        "-p",
+        "--print",
+        type=float,
+        nargs="?",
+        default=None,
+        const=100,
+        metavar="min_score",
+        help="Use after already graded to print assignments with score >= min_score",
     )
-    parser.add_argument('-p', '--print',
-        type=float, nargs="?", default=None, const=100, metavar="min_score",
-        help='Use after already graded to print assignments with score >= min_score'
+    parser.add_argument("--no_output", action="store_true", help="Do not output any code to the console")
+    parser.add_argument(
+        "--generate_config", action="store_true", help="Generate a default config file in the <submission_path>"
     )
-    parser.add_argument('--no_output',
-        action="store_true",
-        help="Do not output any code to the console"
+    parser.add_argument(
+        "-s",
+        "--submissions",
+        action="store",
+        nargs="*",
+        metavar="<submission_name>",
+        default=[],
+        help="Only grade submissions with specified file names (without full path)",
     )
-    parser.add_argument('--generate_config',
-        action="store_true",
-        help="Generate a default config file in the <submission_path>"
-    )
-    parser.add_argument('-s', '--submissions',
-                        action="store", nargs="*", metavar="<submission_name>", default=[],
-                        help="Only grade submissions with specified file names (without full path)"
-    )
-    parser.add_argument('-g', '--guide',
-                        action="store_true",
-                        help="Guide you through setting up a grading environment"
-    )
+    parser.add_argument("-g", "--guide", action="store_true", help="Guide you through setting up a grading environment")
     args = parser.parse_args(argv)
     current_dir = (Path.cwd() / args.submission_path).resolve()
     if args.version:
@@ -55,7 +60,7 @@ def main(argv=None):
             if args.generate_config:
                 grader.generate_config()
             elif args.guide:
-                autograder.guide.main(current_dir, grader)
+                guide.main(current_dir, grader)
             else:
                 return grader.run()
         except AutograderError as e:
