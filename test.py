2cea1ffaddcc2b9b404670b52dc12beb0c5b46a7
Stanislav Zmiev <szmiev2000@gmail.com>
2020-12-22 22:10:05 -0500

    Speed up automatic testing by multiprocessing

--- test.py
+++ test.py
@@ -1,9 +1,30 @@
 #!/usr/bin/env python
 from contextlib import contextmanager
+from multiprocessing.pool import Pool
 from pathlib import Path
 from typing import Callable
 
 import autograder
+
+
+class NonDaemonPool(Pool):
+    def Process(self, *args, **kwds):
+        proc = super(NonDaemonPool, self).Process(*args, **kwds)
+
+        class NonDaemonProcess(proc.__class__):
+            """Monkey-patch process to ensure it is never daemonized"""
+
+            @property
+            def daemon(self):
+                return False
+
+            @daemon.setter
+            def daemon(self, val):
+                pass
+
+        proc.__class__ = NonDaemonProcess
+        return proc
+
 
 TEST_DIRS = {
     "simplest_c": 100,
@@ -51,17 +72,22 @@ def run_silenced_grader(*args):
     return autograder.__main__.main(["--no_output"] + list(args))
 
 
-def main():
-    for test_dir, expected_result in TEST_DIRS.items():
-        with ErrorHandler(test_dir):
-            real_result = int(run_silenced_grader(f"examples/{test_dir}"))
-            msg = f"CHECKING TEST {test_dir} to equal {expected_result}. Real result: {real_result}"
-            if int(expected_result) == real_result:
-                PASS(msg)
-            else:
-                FAIL(msg)
+def test_example(args):
+    test_dir, expected_result = args
+    if expected_result == "EXTRA_CLI_ARGS":
+        return test_extra_cli_args()
+    with ErrorHandler(test_dir):
+        real_result = int(run_silenced_grader(f"examples/{test_dir}"))
+        msg = f"CHECKING TEST {test_dir} to equal {expected_result}. Real result: {real_result}"
+        if int(expected_result) == real_result:
+            PASS(msg)
+        else:
+            FAIL(msg)
 
-    test_extra_cli_args()
+
+def main():
+    with NonDaemonPool() as pool:
+        pool.map(test_example, [(d, r) for d, r in TEST_DIRS.items()] + [tuple(["", "EXTRA_CLI_ARGS"])])
 
 
 def test_extra_cli_args():
