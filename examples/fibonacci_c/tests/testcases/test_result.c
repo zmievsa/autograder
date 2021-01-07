22bb8f6e2552f5c02eec914fe923499d075439cf
Stanislav Zmiev <szmiev2000@gmail.com>
2021-01-06 20:17:39 -0500

    Update test_result.c

--- examples/fibonacci_c/tests/testcases/test_result.c
+++ examples/fibonacci_c/tests/testcases/test_result.c
@@ -1,20 +1,15 @@
 unsigned long long fibonacci(int n);
 
-int main()
-{
+int main() {
     unsigned long long r1, r2, r3, r4;
     r1 = fibonacci(0);
     r2 = fibonacci(2);
     r3 = fibonacci(8);
     r4 = fibonacci(24);
     int score = 0;
-    if (r1 == 0)
-        score += 25;
-    if (r2 == 1)
-        score += 25;
-    if (r3 == 21)
-        score += 25;
-    if (r4 == 46368)
-        score += 25;
+    if (r1 == 0)     score += 25;
+    if (r2 == 1)     score += 25;
+    if (r3 == 21)    score += 25;
+    if (r4 == 46368) score += 25;
     RESULT(score);
 }
