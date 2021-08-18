34a47ea6337e314ea1808f9527ab21dad89c59ce
Ovsyanka <szmiev2000@gmail.com>
2021-08-18 10:07:41 +0300

    Created stdout-only example dir

---
+++ examples/stdout_only/tests/stdout_formatters.py
@@ -0,0 +1,10 @@
+def test_output(s: str):
+    """This function will format each character both in expected and student's outputs,
+    removing all characters that are not digits.
+
+    Its name has to be the same as the name of the testcase you're formatting.
+    Alternatively, you could specify no stdout formatters or make a default formatter
+    named ALL which will be the default formatter for any testcase for which no specific
+    formatter has been written.
+    """
+    return "".join(filter(lambda c: c.isdigit(), s))
