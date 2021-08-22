673464ba8d19dc8d11d447c9667102c0bd3e3ab6
Stanislav Zmiev <szmiev2000@gmail.com>
2021-08-22 01:53:29 -0400

    Canvas integration draft

--- autograder/canvas/__main__.py
+++ autograder/canvas/__main__.py
@@ -1,21 +1,9 @@
-"""
->>> autograder canvas send -h
-usage: autograder canvas send [-h] [-k [api_key]] [-a [assignment_id]] [-g] [-s] [testing_dir_path]
-
-positional arguments:
-  testing_dir_path       Path to directory that contains student submissions
-
-optional arguments:
-  -h, --help                            show this help message and exit
-  -k, --api_key [api_key]               key associated with the canvas user running autograder (will be asked during run if not provided or already saved)
-  -a, --assignment_id [assignment_id]   id associated with the canvas assignment being graded  (will be asked during run if not provided)
-  -g, --grade_assignments               will put assignment grades for all submissions associated with their test results 
-  -s, --save_api_key_globally           will make autograder remember the canvas api key for future grading
-"""
-
 import sys
 from pathlib import Path
 import argparse
+
+from .auth import Token, FILE_WITH_TOKEN_INFO
+from .submissions import upload_results
 
 
 def main(argv=None):
@@ -34,7 +22,7 @@ def _create_parser():
 
 
 def _create_upload_parser(subparsers: argparse._SubParsersAction):
-    parser = subparsers.add_parser("upload", help="Upload assignment grades to canvas")
+    parser = subparsers.add_parser("upload", help="Upload assignments back to canvas")
     parser.add_argument(
         "submission_path",
         type=Path,
@@ -42,21 +30,53 @@ def _create_upload_parser(subparsers: argparse._SubParsersAction):
         default=Path.cwd(),
         help="Path to directory that contains student submissions",
     )
+    parser.add_argument("course_id", type=int)
+    parser.add_argument("assignment_id", type=int)
+    parser.add_argument(
+        "-g",
+        "--grade",
+        action="store_true",
+        help="Also upload grades for each student submission.",
+    )
 
 
 def _create_api_key_parser(subparsers: argparse._SubParsersAction):
-    parser = subparsers.add_parser("api_key", help="Create/Save/Get/Delete api_key")
-    parser.add_argument("url", help="Canvas url")  # TODO: Change its help
+    parser = subparsers.add_parser("token", help="Create/Save/Get/Delete api_key")
     group = parser.add_mutually_exclusive_group()
 
-    group.add_argument("-c", "--create", action="store_true", help="Create a new canvas api key and save it.")
+    group.add_argument(
+        "-c",
+        "--create",
+        action="store",
+        metavar="<base_url>",
+        help="Create a new canvas api key and save it.",
+    )
     group.add_argument("-g", "--get", action="store_true", help="Print the api key saved for the specified url.")
     group.add_argument("-d", "--delete", action="store_true", help="Delete the api key saved for the specified url.")
-    group.add_argument("-s", "--save", action="store", metavar="<key>", help="Save the specified api-key globally.")
+    group.add_argument(
+        "-s",
+        "--save",
+        action="store",
+        nargs=2,
+        metavar=("<base_url>", "<token>"),
+        help="Save the specified api-token globally.",
+    )
 
 
 def _evaluate_args(args):
-    pass
+    if args.api_key_command == "token":
+        if args.create:
+            return Token.create(args.create, FILE_WITH_TOKEN_INFO)
+        elif args.save:
+            Token(args.save[0], args.save[1], FILE_WITH_TOKEN_INFO).save()
+        elif args.get:
+            token = Token.from_file(FILE_WITH_TOKEN_INFO)
+            print(f"Base url: {token.base_url}, Token: {token.token}")
+            return token
+        elif args.delete:
+            Token.from_file(FILE_WITH_TOKEN_INFO).delete()
+    elif args.api_key_command == "upload":
+        upload_results(args.submission_path, args.course_id, args.assignment_id, args.grade)
 
 
 if __name__ == "__main__":
