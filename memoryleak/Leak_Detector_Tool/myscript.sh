319e76dc8dcfcfafd795a8e1e1e529e65793b26b
Faizar Ali <alifaizar@gmail.com>
2021-10-09 21:19:41 -0400

    Modified bash script to work with both C and C++ source code

--- memoryleak/Leak_Detector_Tool/myscript.sh
+++ memoryleak/Leak_Detector_Tool/myscript.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 file=$1
-gcc -c leak_detector_c.c
-gcc -c $file
-gcc -o memtest leak_detector_c.o ${file%.*}.o
-./memtest > /dev/null 2>&1
-cat leak_info.txt
-rm *.o
+
+case "$file" in
+*.c)
+	gcc -c leak_detector_c.c
+	gcc -c $file
+	gcc -o memtest leak_detector_c.o ${file%.*}.o
+	./memtest > /dev/null 2>&1
+	cat leak_info.txt
+	rm *.o
+	;;
+*.cpp)
+	g++ -c leak_detector_c.c 
+	g++ -c $file
+	g++ -o memtest leak_detector_c.o ${file%.*}.o
+	./memtest > /dev/null 2>&1
+	cat leak_info.txt
+	rm *.o
+    ;;
+*)
+	echo "Invalid source file name"
+	exit 1
+esac
