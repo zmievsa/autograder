67d7ae05330eb81f71451c58f2b5da7964f7ec75
Faizar Ali <alifaizar@gmail.com>
2021-10-09 21:35:15 -0400

    Fix bash script to align with better indention style

--- memoryleak/Leak_Detector_Tool/myscript.sh
+++ memoryleak/Leak_Detector_Tool/myscript.sh
@@ -3,22 +3,22 @@ file=$1
 
 case "$file" in
 *.c)
-	gcc -c leak_detector_c.c
-	gcc -c $file
-	gcc -o memtest leak_detector_c.o ${file%.*}.o
-	./memtest > /dev/null 2>&1
-	cat leak_info.txt
-	rm *.o
-	;;
+  gcc -c leak_detector_c.c
+  gcc -c $file
+  gcc -o memtest leak_detector_c.o ${file%.*}.o
+  ./memtest > /dev/null 2>&1
+  cat leak_info.txt
+  rm *.o
+  ;;
 *.cpp)
-	g++ -c leak_detector_c.c 
-	g++ -c $file
-	g++ -o memtest leak_detector_c.o ${file%.*}.o
-	./memtest > /dev/null 2>&1
-	cat leak_info.txt
-	rm *.o
-    ;;
+  g++ -c leak_detector_c.c
+  g++ -c $file
+  g++ -o memtest leak_detector_c.o ${file%.*}.o
+  ./memtest > /dev/null 2>&1
+  cat leak_info.txt
+  rm *.o
+  ;;
 *)
-	echo "Invalid source file name"
-	exit 1
+  echo "Invalid source file name"
+  exit 1
 esac
