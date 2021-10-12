8ba01cb6d76a6bf3dce601eb2ae7df4a3c31e93e
Faizar Ali <54734200+faizarali@users.noreply.github.com>
2021-10-11 20:34:43 -0400

    Update test.c

--- autograder/memory_leak/extra_source_codes/test.c
+++ autograder/memory_leak/extra_source_codes/test.c
@@ -11,7 +11,6 @@ int main(void) {
 
     ptr1 = (char *) malloc (10); // allocating 10 bytes        
     ptr2 = (int *) calloc (10, sizeof(int)); 	// allocating 40 bytes 
-                    // let sizeof int =  4 bytes)
     ptr3 = (float *) calloc (15, sizeof(float)); // allocating 60 bytes
     printf("Hello World\n");
     free(ptr2);
