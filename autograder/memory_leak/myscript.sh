8cfc462be63b187b67ed23c0bc5556b2e521029e
Faizar Ali <alifaizar@gmail.com>
2021-10-13 00:40:37 -0400

    Fixed script to better handle directories

--- autograder/memory_leak/myscript.sh
+++ autograder/memory_leak/myscript.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 file=$1
 
-case "$file" in
+cp $file tmp.${file##*.}
+temp=tmp.${file##*.}
+
+case "$temp" in
 *.c)
     gcc -c leak_detector_c.c
-    gcc -c $file
-    gcc -o memtest leak_detector_c.o ${file%.*}.o
+    gcc -c $temp
+    gcc -o memtest leak_detector_c.o ${temp%.*}.o
     ./memtest > /dev/null 2>&1
     cat leak_info.txt
-    rm memtest leak_detector_c.o ${file%.*}.o
+    rm memtest leak_detector_c.o ${temp%.*}.o $temp
     ;;
 *.cpp | *.c++ | *.cc | *.cxx | *.CPP | *.cp | *.C)
     g++ -c leak_detector_c.c
-    g++ -c $file
-    g++ -o memtest leak_detector_c.o ${file%.*}.o
+    g++ -c $temp
+    g++ -o memtest leak_detector_c.o ${temp%.*}.o
     ./memtest > /dev/null 2>&1
     cat leak_info.txt
-    rm memtest leak_detector_c.o ${file%.*}.o
+    rm memtest leak_detector_c.o ${temp%.*}.o $temp
     ;;
 *)
     echo "Invalid source file name"
