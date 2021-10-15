d81ce73d3363a98c5b80283fad7b66abc88af7d7
Faizar Ali <alifaizar@gmail.com>
2021-10-14 21:14:46 -0400

    Further simplified script

--- autograder/memory_leak/myscript.sh
+++ autograder/memory_leak/myscript.sh
@@ -9,19 +9,17 @@ case "$file" in
     gcc -c leak_detector_c.c
     gcc -c $file
     gcc -o memtest leak_detector_c.o ${file%.*}.o
-    ./memtest > /dev/null 2>&1
-    cat leak_info.txt
-    rm memtest leak_detector_c.o ${file%.*}.o $file
     ;;
 *.cpp | *.c++ | *.cc | *.cxx | *.CPP | *.cp | *.C)
     g++ -c leak_detector_c.c
     g++ -c $file
     g++ -o memtest leak_detector_c.o ${file%.*}.o
-    ./memtest > /dev/null 2>&1
-    cat leak_info.txt
-    rm memtest leak_detector_c.o ${file%.*}.o $file
     ;;
 *)
     echo "Invalid source file name"
     exit 1
 esac
+
+./memtest > /dev/null 2>&1
+cat leak_info.txt
+rm memtest leak_detector_c.o ${file%.*}.o $file
