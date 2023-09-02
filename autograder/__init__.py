642cd443877ae1017dd576035859a9d1c0756082
Stanislav Zmiev <szmiev2000@gmail.com>
2023-09-02 21:16:00 +0400

    Change email

--- autograder/__init__.py
+++ autograder/__init__.py
@@ -5,5 +5,5 @@ __version__ = importlib.metadata.version("autograder")
 __title__ = "autograder"
 __description__ = "Automatic assignment grading for instructor use in programming courses"
 __author__ = "Stanislav Zmiev"
-__author_email__ = "szmiev2000@gmail.com"
+__author_email__ = "zmievsa@gmail.com"
 __license__ = "GPL-3.0"
